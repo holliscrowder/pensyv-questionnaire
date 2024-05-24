671e5d0322fa60da3d28efe832c23bcda997b558
Hollis Crowder <hollis.crowder@gmail.com>
2024-05-24 14:08:20 -0700

    add signup and checksession resources

--- server/app.py
+++ server/app.py
@@ -3,8 +3,9 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, jsonify, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
@@ -19,6 +20,46 @@ from models import User, Question, Questionnaire
 def index():
     return '<h1>Project Server</h1>'
 
+class Signup(Resource):
+
+    def post(self):
+        json = request.get_json()
+        try:
+            # create new user
+            user = User(
+                username = json.get("username"),
+                email = json.get("email")
+            )
+
+            # update session info
+            session["user_id"] = user.id
+            db.session.add(user)
+            db.session.commit()
+            
+            # return user info
+            user_response = jsonify(user.to_dict())
+            return make_response(user_response, 201)
+        except IntegrityError:
+            return make_response({"error": "Database relational integrity error."}, 422)
+        except ValueError:
+            return make_response({"error": "User information value invalid"}, 422)
+
+class CheckSession(Resource):
+    def get(self):
+        # check if session has user ID
+        if session["user_id"]:
+            # create user based on unique user ID
+            user = User.query.filter(User.id == session["user_id"]).first()
+            
+            # return user info
+            user_response = jsonify(user.to_dict())
+            return make_response(user_response, 200)
+        
+        return make_response({"error": "Unauthorized"}, 401)
+
+    
+api.add_resource(Signup, "/signup", endpoint = "signup")
+api.add_resource(CheckSession, "/check_session", endpoint = "check_session")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
